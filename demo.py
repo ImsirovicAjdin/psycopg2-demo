b481d46c683b56402a5d94fb463f106e42e70cd6
Ajdin Imsirovic <ajdika@live.com>
2021-05-14 06:34:35 +0200

    Extract functionality into variables

--- demo.py
+++ demo.py
@@ -4,40 +4,51 @@ connection = psycopg2.connect('dbname=example')
 
 cursor = connection.cursor()
 
-# cursor.execute('''
-#     CREATE TABLE table3 (
-#         id INTEGER PRIMARY KEY,
-#         completed BOOLEAN NOT NULL DEFAULT False
-#     );
-# ''')
+# # cursor.execute('''
+# #     CREATE TABLE table3 (
+# #         id INTEGER PRIMARY KEY,
+# #         completed BOOLEAN NOT NULL DEFAULT False
+# #     );
+# # ''')
 
-# cursor.execute('INSERT INTO table3 (id, completed) VALUES (1, true);')
+# # cursor.execute('INSERT INTO table3 (id, completed) VALUES (1, true);')
 
-cursor.execute('INSERT INTO table3 (id, completed) VALUES (%s, %s);', (2, True))
+# cursor.execute('INSERT INTO table3 (id, completed) VALUES (%s, %s);', (2, True))
 
-cursor.execute('INSERT INTO table3 (id, completed)' +
-' VALUES (%(id)s, %(completed)s);', {
-    'id': 3,
+# cursor.execute('INSERT INTO table3 (id, completed)' +
+# ' VALUES (%(id)s, %(completed)s);', {
+#     'id': 3,
+#     'completed': False
+# })
+
+# connection.commit()
+
+# connection.close()
+# cursor.close()
+
+# # in bash: conda deactivate
+# # python3 demo.py
+# # psql example
+# # \dt
+# # // verify that table3 is there
+# # \d table 3
+# # select * from table3;
+
+# # We can use string interpolation to compose an SQL query using python strings, in two ways:
+# # 1. Using `%s`, passing in a tuple as a 2nd argument in `cursor.execute()`
+# # 2. Using named string parameters %(foo)s, passing in a dictionary instead
+# # in bash, run it etc: python3 demo.py
+# # psql example
+# # example=# select * from table3;
+
+cursor.execute('INSERT INTO table3 (id, completed) VALUES (%s, %s);', (4, True))
+
+SQL = 'INSERT INTO table3 (id, completed) VALUES (%(id),s, %(completed)s);'
+
+data = {
+    'id': 2,
     'completed': False
-})
+}
+cursor.execute(SQL, data)
 
-connection.commit()
-
-connection.close()
-cursor.close()
-
-# in bash: conda deactivate
-# python3 demo.py
-# psql example
-# \dt
-# // verify that table3 is there
-# \d table 3
-# select * from table3;
-
-# We can use string interpolation to compose an SQL query using python strings, in two ways:
-# 1. Using `%s`, passing in a tuple as a 2nd argument in `cursor.execute()`
-# 2. Using named string parameters %(foo)s, passing in a dictionary instead
-# in bash, run it etc: python3 demo.py
-# psql example
-# example=# select * from table3;
-
+# note: the above code works only if some other (commented) parts work
